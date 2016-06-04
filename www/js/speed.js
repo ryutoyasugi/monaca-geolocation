846b22485f979aba9f48c363c053f2a82e5a806a
ryutoyasugi <ry86tr2662@gmail.com>
2016-06-05 01:24:28 +0900

    移動速度表示機能の実装

---
+++ www/js/speed.js
@@ -0,0 +1,26 @@
+ons.bootstrap(); // アプリの初期化
+
+ons.ready(function() { // アプリ起動時の処理
+  setInterval(getSpeed, 1000);
+});
+
+function getSpeed(position) {
+  var options = {
+    enableHighAccuracy: true
+  };
+  navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
+};
+
+function onSuccess(position) {
+  var speed = position.coords.speed;
+  if(speed === -1) {
+    $('#speed').text(0.0);
+  } else {
+    $('#speed').text((speed * 3.6).toFixed(1));
+  }
+};
+function onError(error) {
+  ons.notification.alert({
+    message: error.code + ' ' + error.message
+  });
+};
